aec883d38fa9e7eb98c8e40f2d78e4cf1be07c20
HanzlaKhalid <53270755+HanzlaKhalid@users.noreply.github.com>
2019-07-25 19:16:22 +0500

    user value edit

---
+++ script.js
@@ -0,0 +1,4 @@
+var cities = [", "];
+cities.unshift(prompt("Enter your counrty"));
+cities.push(prompt("Enter your city"));
+alert("Your country is " + cities[0] + cities[1] + "Your city is " + cities[2]);
